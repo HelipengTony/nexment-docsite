61d1b73199a7dbb7bef77229186155d05fefb1b2
HelipengTony <tony.hlp@hotmail.com>
2020-08-23 17:45:26 +0800

    fix: last problem

--- src/components/NexmentDiv.tsx
+++ src/components/NexmentDiv.tsx
@@ -1,3 +1,5 @@
 import dynamic from "next/dynamic";
-const NexmentDiv = dynamic(() => import("./Nexment"));
+const NexmentDiv = dynamic(() => import("./Nexment"), {
+  ssr: false,
+});
 export default NexmentDiv;
